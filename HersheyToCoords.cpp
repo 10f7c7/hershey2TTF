5fae850f5d8455a044e88c5bd699d605a3de550c
10f7c7 <10f7c7@gmail.com>
2022-12-12 13:57:35 -0500

    Decoded Hershey's font into points & Coords

---
+++ HersheyToCoords.cpp
@@ -0,0 +1,101 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int getCoordPoint(char in)  {
+    return int(in) - 'R';
+}
+
+int getLength(string in)  {
+    int len = 0;
+    for (int x = 0; x < in.length(); x++) {
+        if (in[x] == ' ' & in[x + 1] == 'R')  {
+            len -= 2;
+        }
+        // if (in[x] == 'R' && in[x - 1] == ' ')  {
+        //     continue;
+        // }
+        len++;
+    }
+    return len;
+}
+
+void display(int** a, int r, int c, bool HmPts){
+    if (!HmPts)  {
+        cout << '[';
+        for(int i=0; i<r; i++){
+            if (a[i][0] == 'r' & a[i][1] == 'l')  {
+                cout << "nL, ";
+                break;
+            }
+            // for(int j=0; j<c; j++){
+            cout << '[';
+            cout << a[i][0] << ", ";
+            cout << a[i][1] << "]";
+            if (i != r-1)  {
+                cout << ", ";
+            }
+
+            // }
+            // cout << endl;
+        }
+        cout << ']';
+    }
+    if (HmPts) {
+        for(int i=0; i<r; i++){
+        // for(int j=0; j<c; j++){
+            cout << '(';
+            cout << a[i][0] << ", ";
+            cout << a[i][1] << ")";
+        // }
+        cout << endl;
+        }
+    }
+}
+
+
+int main()  {
+    // const string txt = "JZNFNJ RVFVJ";
+    const string txt = "I[VZT[P[NZMXMPNNPMTMVNWPWRMT";
+    const int left = getCoordPoint(txt[0]);
+    const int right = getCoordPoint(txt[1]);
+    // cout << left << endl;
+
+    // const int pairCount = getLength(txt)/2;
+
+
+    int pairNum = 0;
+    // vector<int> coordPairs(getLength(txt)/2);
+
+    int** coordPairs;
+    int row = (txt.length()/2) - 1;
+    int col = 2;
+    coordPairs = new int*[row];
+    for(int i=0; i<row; i++){
+        coordPairs[i] = new int[col];
+    }
+
+    for (int i = 2; i < txt.length(); i++)  {
+        if (txt[i] == ' ' & txt[i + 1] == 'R')  {
+            i += 2;
+            coordPairs[pairNum][0] = 'n';
+            coordPairs[pairNum++][1] = 'l';
+        }
+        // cout << txt[i];
+        // if (txt[i] == 'R' && txt[i - 1] == ' ')  {
+        //     i++;
+        // }
+        // int pair[2];
+
+        // pair[0] = getCoordPoint(txt[i++]);
+        // pair[1] = getCoordPoint(txt[i++]);
+
+        coordPairs[pairNum][0] = getCoordPoint(txt[i++]) + right;
+        coordPairs[pairNum++][1] = -(getCoordPoint(txt[i])) + 9;
+
+    }
+
+    display(coordPairs, row, col, false);
+
+
+}
