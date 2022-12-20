f5f7e88e13f884bba809a2c35c35ba3af3b82320
10f7c7 <10f7c7@gmail.com>
2022-12-20 16:34:44 -0500

    fixed write to file & changed src from .cpp to .h

--- HersheyToCoords.cpp
+++ HersheyToCoords.cpp
@@ -12,9 +12,6 @@ int getLength(string in)  {
         if (in[x] == ' ' & in[x + 1] == 'R')  {
             len -= 2;
         }
-        // if (in[x] == 'R' && in[x - 1] == ' ')  {
-        //     continue;
-        // }
         len++;
     }
     return len;
@@ -24,11 +21,6 @@ void display(int** a, int r, int c, bool HmPts){
     if (!HmPts)  {
         cout << '[';
         for(int i=0; i<r; i++){
-            if (a[i][0] == 'r' & a[i][1] == 'l')  {
-                cout << "nL, ";
-                break;
-            }
-            // for(int j=0; j<c; j++){
             cout << '[';
             cout << a[i][0] << ", ";
             cout << a[i][1] << "]";
@@ -43,7 +35,6 @@ void display(int** a, int r, int c, bool HmPts){
     }
     if (HmPts) {
         for(int i=0; i<r; i++){
-        // for(int j=0; j<c; j++){
             cout << '(';
             cout << a[i][0] << ", ";
             cout << a[i][1] << ")";
@@ -53,34 +44,34 @@ void display(int** a, int r, int c, bool HmPts){
     }
 }
 
-bool writeCoordsToFile(int** arr)  {
-
-    int ptAmt = sizeof(arr) / sizeof(int);
+bool writeCoordsToFile(int** arr, int r)  {
+    int rows = 1;
+    int ptAmt = r;
+    cout << endl << ptAmt << endl;
     ofstream CoordH("newstroke_coords.h");
-    CoordH << "const int** newstroke_coords[] = {" << endl;
-
+    CoordH << ("const int newstroke_coords[%d][650] = {",rows) << endl;
+    CoordH << '{';
     for (int i = 0; i<ptAmt; i++)  {
-        CoordH << arr[i] << ',' << endl;
+        if (i == ptAmt - 1)  {
+            CoordH << arr[i][0] << ", ";
+            CoordH << arr[i][1];
+            break;
+        }
+        CoordH << arr[i][0] << ", ";
+        CoordH << arr[i][1] << ", ";
     }
-
+    CoordH << '}' << endl;
     CoordH << "};";
     CoordH.close();
 }
 
 
 int main()  {
-    // const string txt = "JZNFNJ RVFVJ";
-    const string txt = "I[VZT[P[NZMXMPNNPMTMVNWPWRMT";
+    const string txt = "I\\WMW^V`UaSbPbNa RWZU[Q[OZNYMWMQNOONQMUMWN";
     const int left = getCoordPoint(txt[0]);
     const int right = getCoordPoint(txt[1]);
-    const bool HumanCoord = true;
-    // cout << left << endl;
-
-    // const int pairCount = getLength(txt)/2;
-
-
+    const bool HumanCoord = false;
     int pairNum = 0;
-    // vector<int> coordPairs(getLength(txt)/2);
 
     int** coordPairs;
     int row = (txt.length()/2) - 1;
@@ -96,27 +87,16 @@ int main()  {
             coordPairs[pairNum][0] = 'n';
             coordPairs[pairNum++][1] = 'l';
         }
-        // cout << txt[i];
-        // if (txt[i] == 'R' && txt[i - 1] == ' ')  {
-        //     i++;
-        // }
-        // int pair[2];
-
-        // pair[0] = getCoordPoint(txt[i++]);
-        // pair[1] = getCoordPoint(txt[i++]);
 
         coordPairs[pairNum][0] = getCoordPoint(txt[i++]) + right;
-        coordPairs[pairNum++][1] = -(getCoordPoint(txt[i])) + 9;
+        coordPairs[pairNum++][1] = -(getCoordPoint(txt[i])) + 9 + 7;
 
     }
 
     display(coordPairs, row, col, HumanCoord);
 
     if (!HumanCoord)  {
-        writeCoordsToFile(coordPairs);
+        writeCoordsToFile(coordPairs, row);
     }
-
-
-
 
 }
